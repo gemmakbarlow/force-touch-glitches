c0e33403ce1e77fc2069aa747f0f951dfe02cb47
Gemma Barlow <gemmakbarlow@gmail.com>
2016-04-27 21:25:42 -0400

    Added 'hint' label

--- force-touch-glitches/force-touch-glitches/ViewController.swift
+++ force-touch-glitches/force-touch-glitches/ViewController.swift
@@ -12,10 +12,35 @@ import GlitchLabel
 class ViewController: UIViewController {
 
     @IBOutlet weak var glitchingLabel: GlitchLabel!
+    @IBOutlet weak var hintLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        hintLabel.alpha = 0.0
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animateInOut(true) { [weak self] completed in
+            if completed {
+                self?.animateInOut(false, completion: nil)
+            }
+        }
+    }
+    
+    private func animateInOut(animateIn: Bool, completion: ((Bool) -> Void)?) {
+        UIView.animateWithDuration(1.0, delay: 2.0, options: .BeginFromCurrentState, animations: { [weak self] in
+            self?.hintLabel.alpha = animateIn ? 1.0 : 0.0
+        }) { completed in
+            completion?(completed)
+        }
     }
 
     override func didReceiveMemoryWarning() {

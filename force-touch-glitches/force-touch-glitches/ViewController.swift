4705f450b5b7973821643c2a12583fc9ab6c1159
Gemma Barlow <gemmakbarlow@gmail.com>
2016-04-30 16:48:31 -0400

    Hooked up a Force Touch detector to the Glitch Label

--- force-touch-glitches/force-touch-glitches/ViewController.swift
+++ force-touch-glitches/force-touch-glitches/ViewController.swift
@@ -8,20 +8,25 @@
 
 import UIKit
 import GlitchLabel
+import DFContinuousForceTouchGestureRecognizer
+import AudioToolbox.AudioServices
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var glitchingLabel: GlitchLabel!
     @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var target: UIView!
+    
+    // MARK: - Memory Management
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupForceTouchGestureRecognizer()
+        glitchingLabel.glitchEnabled = false
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
         hintLabel.alpha = 0.0
     }
     
@@ -35,6 +40,14 @@ class ViewController: UIViewController {
         }
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Animation
+    
     private func animateInOut(animateIn: Bool, completion: ((Bool) -> Void)?) {
         UIView.animateWithDuration(1.0, delay: 2.0, options: .BeginFromCurrentState, animations: { [weak self] in
             self?.hintLabel.alpha = animateIn ? 1.0 : 0.0
@@ -43,11 +56,31 @@ class ViewController: UIViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setupForceTouchGestureRecognizer() {
+        let forceTouch = DFContinuousForceTouchGestureRecognizer()
+        forceTouch.forceTouchDelegate = self
+        target.addGestureRecognizer(forceTouch)
     }
 
+}
 
+
+func vibrate() {
+    AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+}
+
+extension ViewController: DFContinuousForceTouchDelegate {
+    
+    
+    func forceTouchRecognized(recognizer: DFContinuousForceTouchGestureRecognizer) {
+        vibrate()
+        glitchingLabel.glitchEnabled = true
+        
+    }
+
+    func forceTouchRecognizer(recognizer: DFContinuousForceTouchGestureRecognizer!, didEndWithForce force: CGFloat, maxForce: CGFloat) {
+        glitchingLabel.glitchEnabled = false
+    }
+    
 }
 

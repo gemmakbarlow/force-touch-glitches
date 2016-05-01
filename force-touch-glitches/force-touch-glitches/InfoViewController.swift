1a64e9bd000adc5533954a3b1c3d39f81d66cec3
Gemma Barlow <gemmakbarlow@gmail.com>
2016-05-01 10:07:37 -0400

    Added in credits / info UIViewController.

---
+++ force-touch-glitches/force-touch-glitches/InfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.swift
+//  force-touch-glitches
+//
+//  Created by Gemma Barlow on 5/1/16.
+//  Copyright © 2016 gemmakbarlow. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    @IBAction func closeButtonTapped(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
